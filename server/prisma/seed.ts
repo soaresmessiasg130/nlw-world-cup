ec9ebf635a38b63c6d03f77faf773b210182dbe7
soaresmessiasg130 <soaresmessiasg130@gmail.com>
2022-11-04 13:58:22 -0300

    server: add prisma seed

---
+++ server/prisma/seed.ts
@@ -0,0 +1,58 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      name: "Fulano de Tal",
+      email: "fulanodetal@email.com",
+      avatarUrl: "https://github.com/soaresmessiasg130.png",
+    },
+  });
+
+  const pool = await prisma.pool.create({
+    data: {
+      title: "Bolão da Fase de Grupos",
+      code: "BOLAO123",
+      ownerId: user.id,
+      participants: {
+        create: {
+          userId: user.id,
+        },
+      },
+    },
+  });
+
+  await prisma.game.create({
+    data: {
+      date: "2022-12-02T12:00:00.201Z",
+      firstTeamCountryCode: "DE",
+      secondTeamCountryCode: "BR",
+    },
+  });
+
+  await prisma.game.create({
+    data: {
+      date: "2022-12-02T12:00:00.201Z",
+      firstTeamCountryCode: "BR",
+      secondTeamCountryCode: "AR",
+      guesses: {
+        create: {
+          firstTeamPoints: 2,
+          secondTeamPoints: 1,
+          participant: {
+            connect: {
+              userId_poolId: {
+                userId: user.id,
+                poolId: pool.id,
+              },
+            },
+          },
+        },
+      },
+    },
+  });
+}
+
+main();

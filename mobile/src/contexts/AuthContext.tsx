7a285c6f673ea293230002f9e29141ed36620782
soaresmessiasg130 <soaresmessiasg130@gmail.com>
2022-11-06 16:20:10 -0300

    mobile: getting auth token

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -1,24 +1,56 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import * as AuthSession from "expo-auth-session";
+import * as WebBrowser from "expo-web-browser";
+import * as Google from "expo-auth-session/providers/google";
 
 export const AuthContext = createContext({} as AuthContextDataProps);
 
 export default function AuthContextProvider({ children }) {
+  const [isUserLoading, setIsUserLoading] = useState(false);
   const [user, setUser] = useState<UserProps>();
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    clientId:
+      "749902299484-54e2fj0lstn1t61edi7grctju5u97sed.apps.googleusercontent.com",
+    redirectUri: AuthSession.makeRedirectUri({ useProxy: true }),
+    scopes: ["email", "profile"],
+  });
+
+  useEffect(() => {
+    async function loadSource() {
+      if (
+        response?.type === "success" &&
+        response?.authentication?.accessToken
+      ) {
+        await signInWithGoogle(response.authentication.accessToken);
+      }
+    }
+
+    loadSource();
+  }, [response]);
 
   async function signIn() {
-    setUser({
-      name: "Messias Soares",
-      avatarUrl: "https://github.com/soaresmessiasg130.png",
-    });
+    try {
+      setIsUserLoading(true);
 
-    console.log("Vamos logar!!!");
+      await promptAsync();
+    } catch (error) {
+      console.log(error);
+
+      throw error;
+    } finally {
+      setIsUserLoading(false);
+    }
+  }
+
+  async function signInWithGoogle(accessToken: string) {
+    console.log("ACCESS TOKEN => ", accessToken);
   }
 
   return (
     <AuthContext.Provider
       value={{
         signIn,
+        isUserLoading,
         user,
       }}
     >
@@ -29,6 +61,7 @@ export default function AuthContextProvider({ children }) {
 
 export interface AuthContextDataProps {
   user: UserProps;
+  isUserLoading: boolean;
   signIn: () => Promise<void>;
 }
 
